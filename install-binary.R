26a3b5b62d92a87772c65b92df6611c0000d79a7
Tom Palmer <remlapmot@hotmail.com>
2025-05-28 15:43:49 +0100

    Don't install sessioninfo
    Because sessioninfo depends on gh which now requires R 4.1

--- install-binary.R
+++ install-binary.R
@@ -12,7 +12,7 @@ if (Sys.info()["sysname"] != "Linux") {
   # Windows and macOS
   pak::repo_add(CRAN = "https://cloud.r-project.org")
   # pak::repo_add(universe = "https://mrcieu.r-universe.dev")
-  pak::pkg_install(c("tmsens", "sessioninfo"), dependencies = TRUE)
+  pak::pkg_install(c("tmsens"), dependencies = TRUE)
 } else if (Sys.info()["machine"] == "x86_64") {
   # Linux x86_64
   if (getRversion() >= '4.5.0') {
@@ -22,14 +22,14 @@ if (Sys.info()["sysname"] != "Linux") {
   }
   pak::repo_add(CRAN = "https://p3m.dev/cran/__linux__/noble/latest")
   pak::repo_add(CRANbackup = "https://cloud.r-project.org")
-  pak::pkg_install(c("tmsens", "sessioninfo"), dependencies = TRUE)
+  pak::pkg_install(c("tmsens"), dependencies = TRUE)
 } else if (Sys.info()["machine"] == "aarch64") {
   # Linux aarch64
   pak::repo_add(CRAN = "https://cloud.r-project.org")
   # pak::repo_add(universe = "https://mrcieu.r-universe.dev")
-  pak::pkg_install(c("tmsens", "sessioninfo"), dependencies = TRUE)
+  pak::pkg_install(c("tmsens"), dependencies = TRUE)
 }
 
 library(tmsens)
 
-sessioninfo::session_info(pkgs = "installed")
+utils::sessionInfo()
